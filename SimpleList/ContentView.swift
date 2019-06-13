86bec4c570803f634f4365dbf3cd495cc348a1f0
Dawid Czmyr <dczmyr@bluesoft.net.pl>
2019-06-13 02:01:26 +0200

    add NavigationView, new SwiftUI method ForEach to populate data source

--- SimpleList/ContentView.swift
+++ SimpleList/ContentView.swift
@@ -11,25 +11,40 @@ import SwiftUI
 struct Person: Identifiable {
     var id = UUID()
     let name: String
+    let gender: String
+}
+
+struct Persons {
+    let firstPerson = Person(name: "Dawid", gender: "male")
+    let secondPeson = Person(name: "Kamil", gender: "male")
+    let thirdPerson = Person(name: "Łukasz", gender: "male")
+    var persons: [Person] = []
+
+    init() {
+        self.persons = [firstPerson, secondPeson, thirdPerson]
+    }
 }
 
 struct NameRow: View {
     var person: Person
     var body: some View {
-        Text(person.name)
+        Text(person.name + " is a " + person.gender)
     }
 }
 
 struct ContentView : View {
+    var persons = Persons()
+
     var body: some View {
-        let firstPerson = Person(name: "Dawid")
-        let secondPeson = Person(name: "Kamil")
-        let thirdPerson = Person(name: "Łukasz")
+        NavigationView {
+            List {
 
-        let personArray = [firstPerson, secondPeson, thirdPerson]
-
-        return List(personArray) { person in
-            NameRow(person: person)
+                Section(header: Text("Section 1"), content: {
+                    ForEach(persons.persons.identified(by: \.id)) { person in
+                        NameRow(person: person)
+                    }
+                })
+            }.listStyle(.grouped).navigationBarTitle(Text("Simple TableView"))
         }
     }
 }
